5da7fa86061c352cba660e023f929cd57bbf728f
klei <klei>
2019-08-15 16:35:12 +0000

    v359861 - M:2115860716621312667

--- scripts/prefabs/portablespicer.lua
+++ scripts/prefabs/portablespicer.lua
@@ -33,7 +33,7 @@ local prefabs_item =
 
 local function ChangeToItem(inst)
     if inst.components.stewer.product ~= nil and inst.components.stewer:IsDone() then
-        inst.components.lootdropper:SpawnLootPrefab(inst.components.stewer.product)
+        inst.components.stewer:Harvest()
     end
     if inst.components.container ~= nil then
         inst.components.container:DropEverything()
@@ -332,6 +332,8 @@ local function itemfn()
     inst.AnimState:PlayAnimation("idle_ground")
 
     inst:AddTag("portableitem")
+
+    MakeInventoryFloatable(inst, "med")
 
     inst.entity:SetPristine()
 
